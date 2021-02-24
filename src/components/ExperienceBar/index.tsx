3a1d86e48f68a43eb68dc1e76b0b259a7b69cdd9
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-02-24 01:12:22 -0300

    feat: initial app with next js

---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles.css';
+
+const ExperienceBar: React.FC = () => {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '50%' }} />
+        <span className="current-experience" style={{ left: '50%' }}>
+          300 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  )
+}
+
+export default ExperienceBar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import ExperienceBar from '../components/ExperienceBar';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
