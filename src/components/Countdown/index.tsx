f5a7f0415b2f0f81d2602ac1cff5fb2a71c9e4c1
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-02-25 02:40:26 -0300

    countdown hook

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import styles from '../../styles/components/Countdown.module.css';
 
 const Countdown: React.FC = () => {
+  const [time, setTime] = useState(25 * 60)
+  const [active, setActive] = useState(false)
+
+  const minutes = useMemo(() => Math.floor(time / 60), [time])
+  const seconds = useMemo(() => time % 60, [time])
+
+  const [minuteLeft, minuteRight] = useMemo(() => {
+    return String(minutes).padStart(2, '0').split('')
+  }, [minutes])
+
+  const [secondLeft, secondRight] = useMemo(() => {
+    return String(seconds).padStart(2, '0').split('')
+  }, [seconds])
+
+  const startCountdown = useCallback(() => {
+    setActive(true)
+  }, [])
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    }
+  }, [active, time])
+
   return (
     <>
       <div className={styles.countdownContainer}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
 
-      <button type="button" className={styles.countdownButton}>
+      <button
+        type="button"
+        className={styles.countdownButton}
+        onClick={startCountdown}
+      >
         Iniciar um ciclo
       </button>
     </>
