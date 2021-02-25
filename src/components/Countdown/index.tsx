0430231c3aad3e58071346ba2fcb59f5bfcedade
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-02-25 02:20:55 -0300

    added countdown styles

---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from '../../styles/components/Countdown.module.css';
+
+const Countdown: React.FC = () => {
+  return (
+    <>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>2</span>
+          <span>5</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>0</span>
+          <span>0</span>
+        </div>
+      </div>
+
+      <button type="button" className={styles.countdownButton}>
+        Iniciar um ciclo
+      </button>
+    </>
+  )
+}
+
+export default Countdown;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,11 +13,11 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet" />
+
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
         </Head>
 
         <body>
-          <title>Move.it</title>
-
           <Main />
           <NextScript />
         </body>
